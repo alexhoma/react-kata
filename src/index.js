f0bba12fd8cd8934f661deed7f965da51eb4f2ce
alexhoma <alexcm.14@gmail.com>
2019-07-02 15:21:34 +0200

    remove react initial render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import './styles.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => <div>Hello world!</div>;
-
-ReactDOM.render(<App />, document.getElementById('root'));
