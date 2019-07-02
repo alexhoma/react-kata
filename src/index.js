0207ccdcb2ae07f830e3c188aae0d25509691a5e
alexhoma <alexcm.14@gmail.com>
2019-07-02 15:17:57 +0200

    resets example to be more basic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./styles.css";
+import './styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div>Hello world!</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
